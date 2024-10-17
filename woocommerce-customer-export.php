6f139ac6572f850bc7ed95c408e1210b6b7ceab5
Saeid Kasiri <s.k.flower@gmail.com>
2024-10-17 20:47:54 +0330

    Plugin Name

--- woocommerce-customer-export.php
+++ woocommerce-customer-export.php
@@ -1,1 +1,20 @@
 <?php
+
+if (! defined('ABSPATH')) {
+    exit();
+}
+
+/*
+Plugin Name:  Woocommerce Customer Export
+Plugin URI:   https://redeemgate.com
+Description:  Export Woocommerce Customers as you want
+Version:      1.0.0.1
+Author:       Saeidksr
+Author URI:   https://github.com/skasiri/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wcex
+Domain Path:  /
+*/
+
+define('HubGiftApi_PREFIX', 'wcex');
